c3f20a5dd48c3d3a1366d728e8d559d6635d1011
Syed Rakib Al Hasan <975768+syedrakib@users.noreply.github.com>
2020-06-08 13:30:17 +0800

    Revise conditon

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ locals {
   # Cloud NAT --------------------------------------------------------------------------------------
   cloud_router_name        = format("cloud-router-%s", var.name_suffix)
   cloud_nat_name           = format("cloud-nat-%s", var.name_suffix)
-  nat_ip_allocation_policy = length(google_compute_address.manual_nat_ips) > 0 ? "MANUAL_ONLY" : "AUTO_ONLY"
+  nat_ip_allocation_policy = var.num_of_static_nat_ips > 0 ? "MANUAL_ONLY" : "AUTO_ONLY"
   # Google Services Peering ------------------------------------------------------------------------
   g_services_address_name          = format("gservices-address-%s", var.name_suffix)
   g_services_address_ip            = split("/", local.private_secondary_ip_ranges.g_services.ip_cidr_range)[0]

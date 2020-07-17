4008e8c0225deefa8637fd3e8fe31d8a32bd8f11
Syed Rakib Al Hasan <975768+syedrakib@users.noreply.github.com>
2020-07-17 17:03:21 +0800

    New output cloud_nat_ips

--- outputs.tf
+++ outputs.tf
@@ -23,6 +23,11 @@ output "cloud_nat_id" {
   value       = google_compute_router_nat.cloud_nat.id
 }
 
+output "cloud_nat_ips" {
+  description = "External IP addresses created (and assigned to private subnet resources) by Cloud NAT if value of \"var.num_of_static_nat_ips\" is greater than \"0\".."
+  value       = google_compute_address.static_nat_ips.*.address
+}
+
 output "ip_range_name_private_k8s_pods" {
   description = "Name of the private secondary IP range for k8s/GKE pods."
   value       = local.private_secondary_ip_ranges.k8s_pods.range_name

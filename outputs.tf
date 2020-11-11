84ade443884f44c3a021a48018fc1534f88c6137
Syed Rakib Al Hasan <975768+syedrakib@users.noreply.github.com>
2020-11-12 00:55:31 +0800

    Improve output of network_name

--- outputs.tf
+++ outputs.tf
@@ -5,7 +5,7 @@ output "network" {
 
 output "network_name" {
   description = "The generated name of the VPC network."
-  value       = local.vpc_name
+  value       = google_compute_network.vpc.name
 }
 
 output "network_id" {

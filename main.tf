7069bc54c9914df0687b5a10392e2c06ce716eee
Syed Rakib Al Hasan <975768+syedrakib@users.noreply.github.com>
2022-11-16 02:04:41 +0800

    Enforce required_providers
    Tried and Tested with google providers v4.42.0

--- main.tf
+++ main.tf
@@ -1,5 +1,15 @@
 terraform {
   required_version = ">= 0.13.1" # see https://releases.hashicorp.com/terraform/
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 4.42.0" # see https://github.com/terraform-providers/terraform-provider-google/releases
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = ">= 4.42.0" # see https://github.com/terraform-providers/terraform-provider-google-beta/releases
+    }
+  }
 }
 
 locals {
